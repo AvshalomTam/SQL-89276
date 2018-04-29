2a820b1f38b48ec64fc0180664d3e76809654d21
Or <33380165+orrpaz@users.noreply.github.com>
2018-04-30 02:20:41 +0300

    Add files via upload

---
+++ inProgress/q07.sql
@@ -0,0 +1,4 @@
+SELECT count(*) 
+FROM (SELECT name FROM film JOIN film_category 
+ON (film.film_id = film_category.film_id) JOIN category 
+ON (film_category.category_id = category.category_id) GROUP BY name HAVING AVG(replacement_cost-rental_rate) > 17) as a;
