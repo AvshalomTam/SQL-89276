260dcc2086e80b266ff82f2c0ebc3a257389bbbe
Or <33380165+orrpaz@users.noreply.github.com>
2018-04-29 19:54:05 +0300

    Add files via upload

--- inProgress/q04new.sql
+++ inProgress/q04new.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT  c.first_name AS 'First Name',
 c.last_name AS 'Last Name',
 cty.city AS 'City'    
-FROM customer c, address a,city cty,country ct
+FROM customer c, address a, city cty, country ct
 WHERE ct.country_id = cty.country_id
 AND ct.country_id = 20
 AND a.city_id = cty.city_id
