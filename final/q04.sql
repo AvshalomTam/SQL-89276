911601c39f3d52e25de575d94d6fd75f3071901c
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:42:39 +0300

    Delete q04.sql

--- final/q04.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT  c.first_name AS 'First Name',
-c.last_name AS 'Last Name',
-ci.city AS 'City'    
-FROM customer c, address a, city ci, country co 
-WHERE c.address_id = a.address_id 
-AND a.city_id = ci.city_id
-AND ci.country_id = co.country_id
-AND co.country ='Canada'
-ORDER BY last_name, first_name;
