f06940947436105cb2849cceac049f2d32e8701b
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:47:29 +0300

    Delete q10.sql

--- inProgress/q10.sql
+++
@@ -1,9 +0,0 @@
-select first_name as "FN", last_name as "LN"
-from customer 
-join address
-on (customer.address_id = address.address_id)
-join city
-on (address.city_id = city.city_id)
-where city.city ='Arak'
-GROUP BY last_name
-HAVING last_name LIKE '%S';
