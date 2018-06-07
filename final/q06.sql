f46b274efedf9e6bf45dd7a9908e57db279ca70e
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:43:12 +0300

    Delete q06.sql

--- final/q06.sql
+++
@@ -1,9 +0,0 @@
-SELECT country, count(*) AS 'CustomersNum'
-FROM customer c, address a, city ci, country co
-WHERE c.address_id = a.address_id 
-AND a.city_id = ci.city_id 
-AND ci.country_id = co.country_id
-GROUP BY country
-HAVING count(*) > 10 AND count(*) < 20
-ORDER BY count(*) DESC, country;
-
