86edda27b5c239269c059ec83dd0994a743227e5
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:45:02 +0300

    Delete q04new.sql

--- inProgress/q04new.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT  c.first_name AS 'First Name',
-c.last_name AS 'Last Name',
-cty.city AS 'City'    
-FROM customer c, address a, city cty, country ct
-WHERE ct.country_id = cty.country_id
-AND ct.country_id = 20
-AND a.city_id = cty.city_id
-AND c.address_id = a.address_id ORDER BY last_name,first_name;
