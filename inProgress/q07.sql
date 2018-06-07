3578051d13e60e989a3f9ff740afa68396b1b479
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:46:59 +0300

    Delete q07.sql

--- inProgress/q07.sql
+++
@@ -1,4 +0,0 @@
-SELECT count(*) 
-FROM (SELECT name FROM film JOIN film_category 
-ON (film.film_id = film_category.film_id) JOIN category 
-ON (film_category.category_id = category.category_id) GROUP BY name HAVING AVG(replacement_cost-rental_rate) > 17) as x;
