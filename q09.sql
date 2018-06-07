9588e9d56e92a9703b98aff5cca61649aa551c5f
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:40:56 +0300

    Delete q09.sql

--- q09.sql
+++
@@ -1,7 +0,0 @@
-SELECT first_name,last_name 
-FROM customer 
-WHERE customer.first_name = (SELECT first_name FROM actor WHERE actor.actor_id = 8)
-UNION 
-(SELECT first_name,last_name FROM actor WHERE actor.actor_id NOT LIKE 8 
-AND actor.first_name = (SELECT first_name FROM actor WHERE actor.actor_id = 8))
-ORDER BY last_name;
