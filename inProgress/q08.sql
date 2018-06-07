c4585ac443a96ea654b726d1ae83dfff8c479613
Avshalom Tam <32040834+AvshalomTam@users.noreply.github.com>
2018-06-08 01:47:14 +0300

    Delete q08.sql

--- inProgress/q08.sql
+++
@@ -1,3 +0,0 @@
-SELECT distinct last_name,first_name FROM actor
-WHERE last_name IN(SELECT last_name FROM actor GROUP BY last_name HAVING count(*) > 3)
-AND first_name IN(SELECT first_name FROM actor GROUP BY first_name HAVING count(*) = 1);
